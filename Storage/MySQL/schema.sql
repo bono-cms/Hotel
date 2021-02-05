ff67bd1a0b5beaf4087eba8a22861cf99cee2968
dave-yang <daworld.ny@gmail.com>
2021-02-05 16:39:46 +0500

    Initial room mapper

---
+++ Storage/MySQL/schema.sql
@@ -0,0 +1,7 @@
+
+/* Rooms */
+DROP TABLE IF EXISTS `bono_module_hotel_rooms`;
+CREATE TABLE `bono_module_hotel_rooms` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `price` FLOAT NOT NULL
+);
