3a5a17c4a8538dfbd9e46f5db24c4b1757b33ace
dave-yang <daworld.ny@gmail.com>
2021-02-06 13:58:22 +0500

    Initial table and localization mapper

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -4,4 +4,17 @@ DROP TABLE IF EXISTS `bono_module_hotel_rooms`;
 CREATE TABLE `bono_module_hotel_rooms` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `price` FLOAT NOT NULL
-);
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
+
+/* Roooms lozations */
+DROP TABLE IF EXISTS `bono_module_hotel_rooms_translations`;
+CREATE TABLE `bono_module_hotel_rooms_translations` (
+    `id` INT NOT NULL COMMENT 'Room ID',
+    `lang_id` INT NOT NULL COMMENT 'Corresponding Language ID',
+    `name` varchar(254) NOT NULL COMMENT 'Room name',
+    `description` TEXT NOT NULL COMMENT 'Room description',
+
+    FOREIGN KEY (id) REFERENCES bono_module_hotel_rooms(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
+    
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
