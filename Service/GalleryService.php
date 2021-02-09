66272ad41354eb5080ebae49c03d1ea570f93e5b
dave-yang <daworld.ny@gmail.com>
2021-02-09 09:28:57 +0500

    Added common methods

--- Service/GalleryService.php
+++ Service/GalleryService.php
@@ -11,9 +11,11 @@
 
 namespace Hotel\Service;
 
+use Krystal\Stdlib\VirtualEntity;
 use Hotel\Storage\GalleryMapperInterface;
+use Cms\Service\AbstractManager;
 
-final class GalleryService
+final class GalleryService extends AbstractManager
 {
     /**
      * Any-compliant gallery mapper
@@ -31,6 +33,63 @@ final class GalleryService
     public function __construct(GalleryMapperInterface $galleryMapper)
     {
         $this->galleryMapper = $galleryMapper;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function toEntity(array $row)
+    {
+        $entity = new VirtualEntity();
+        $entity->setId($row['id'])
+               ->setRoomId($row['room_id'])
+               ->setOrder($row['order'])
+               ->setFile($row['file']);
+
+        return $entity;
+    }
+
+    /**
+     * Returns last image id
+     * 
+     * @return int
+     */
+    public function getLastId()
+    {
+        return $this->galleryMapper->getMaxId();
+    }
+
+    /**
+     * Saves an image
+     * 
+     * @param array $input Raw input data
+     * @return boolean
+     */
+    public function save(array $input)
+    {
+        return $this->galleryMapper->persist($input['data']['image']);
+    }
+
+    /**
+     * Deletes an image by its id
+     * 
+     * @param int $id Image id
+     * @return boolean
+     */
+    public function deleteById($id)
+    {
+        return $this->galleryMapper->deleteByPk($id);
+    }
+
+    /**
+     * Fetch image entity by its id
+     * 
+     * @param int $id Image id
+     * @return array
+     */
+    public function fetchById($id)
+    {
+        return $this->prepareResult($this->galleryMapper->findByPk($id));
     }
 
     /**
